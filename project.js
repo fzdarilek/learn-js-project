495e98b46455cfd3292d167ff6633d5b7f065f58
phrenkie-caibi <95527652+phrenkie@users.noreply.github.com>
2023-05-08 14:45:09 +0200

    add: steps

--- project.js
+++ project.js
@@ -3,6 +3,8 @@
 // 3. Collect a bet amount
 // 4. Spin the slot machine
 // 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again
 
 const prompt = require("prompt-sync")();
 
